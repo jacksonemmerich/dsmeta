8df0de03974fe67727ddbbc6f3381d699b56b61a
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-07-17 20:50:50 -0400

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import { Toast } from 'react-toastify/dist/components';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
@@ -10,7 +12,7 @@ type Props = {
 function handleClick(id :number){
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response => {
-    console.log("sucesso!")
+    toast.info("SMS enviado com sucesso!")
   });
 }
 
